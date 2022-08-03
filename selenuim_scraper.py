810de58906cda887a67960caf0f033e2c9196e4c
phillchavda <phillipe.chavda@gmail.com>
2022-08-03 11:53:27 -0300

    feat: Working automatic Blaze better implemented for the game Double

--- selenuim_scraper.py
+++ selenuim_scraper.py
@@ -1,15 +1,13 @@
-from email import header
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-import pandas as pd
+from selenium.webdriver import ChromeOptions, Chrome
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 from matplotlib import pyplot as plt
+from requests import post
 
-website = "https://blaze.com/en/games/double?modal=double_history_index"
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) 
 
 def plot_results(color_list):
     num_of_color = {"red": 0, "black": 0, "white": 0}
@@ -56,8 +54,7 @@ def play_double(color_list):
         if your_money == 0:
             break
 
-        print("pick a color (red, black or white)")
-        chosen_color = input()
+        chosen_color = input("pick a color (red, black or white)")
 
         print("place a bet")
         bet = int(input())
@@ -94,40 +91,128 @@ def play_double(color_list):
 
     print("now you have {} dollars" .format(your_money))
 
-def write_to_csv(dictionary):
-    df = pd.DataFrame.from_dict(dictionary, orient='index')
-    df.to_csv('selected_colors.csv', index=False)
+
+def write_to_txt(color_list):
+    with open("selected_colors.txt", "w") as file:
+        for color in color_list:
+            file.write(color)
+            file.write("\n")
+
+
+def send_discord_msg(token, chanel_id, message):
+    discord_url = f"https://discord.com/api/v9/channels/{chanel_id}/messages"
+    data = {"content": message}
+    header = {"authorization":token}
+    post(discord_url, data=data, headers=header)
+
 
 dictionary = {}
-number_of_times = 1
-for round in range(number_of_times):
+max_len_dict = 10000
+number_of_rounds_whitout_white = 100
+number_of_bets_after_white = 10
+investo_counter = 0
+message_counter = 0
+the_colors = []
+seed_before_click = " "
+
+website = "https://blaze.com/en/games/double?modal=double_history_index"
+website2 = "https://blaze.com/en/games/double?modal=auth&tab=login"
+
+email = input("Enter your Blaze acount email")
+password = input("Enter your password")
+
+token = "NzEwNTE3OTQ3MzgyMzY2MjU4.GDn3Cm.Ua8BYaubSl-iJIhBg4tiMomqJfS0gGs2P2hkvM"
+chanel_id = "1004096816335310871"
+
+driver = Chrome(service=Service(ChromeDriverManager().install())) 
+
+options = ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver2 = Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver2.get(website2)
+
+driver2.find_element(By.NAME,"username").send_keys(email)
+driver2.find_element(By.NAME,"password").send_keys(password)
+driver2.find_element(By.CLASS_NAME, "input-footer").click()
+
+sleep(5) #improve
+
+driver2.find_element(By.CLASS_NAME, "input-field").send_keys("4")
+driver2.find_element(By.CLASS_NAME, "white").click()
+
+while(len(dictionary) < max_len_dict):
     driver.get(website)
-    time.sleep(1)
+    sleep(5) #improve
 
-    selected_seeds = driver.find_elements(By.XPATH, ".//tbody/tr/td[3]/div/a")
-    selected_colors = driver.find_elements(By.XPATH, ".//tbody/tr/td[2]/div/div")
+    # Waits for selected colors to load into page
+    WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, ".//tbody/tr/td[3]/div/a")))
+    WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, ".//tbody/tr/td[2]/div/div")))
 
+    # Gets slected colors and their identifiers from webpage
+    selected_seeds = driver.find_elements(By.XPATH, ".//tbody/tr/td[3]/div/a") #improve (sometimes isnt getting all 10 results)
+    selected_colors = driver.find_elements(By.XPATH, ".//tbody/tr/td[2]/div/div") #improve (sometimes isnt getting all 10 results)
+
+    # Cleans up data and makes sure same color hasnt been stored twice in dictionary
     for i in range(9,-1, -1):
         seed = selected_seeds[i].text
-        trash, color = selected_colors[i].get_attribute('class').split(" ")
+        _, color = selected_colors[i].get_attribute('class').split(" ")
 
         if seed not in dictionary:
             dictionary[seed] = color
 
+    # Converts dictionary into 2 lists for ease of use
+    the_colors = list(dictionary.values())
+    the_seeds = list(dictionary.keys())
 
-    if round != number_of_times - 1:
-        time.sleep(20)
+    last_seed = the_seeds[-1]
 
+    # Will bet of white if the number of rounds without white chosen has benn surpased
+    if len(dictionary) > number_of_rounds_whitout_white and "white" not in the_colors[-number_of_rounds_whitout_white:]:
+        if seed_before_click != last_seed:
+            sleep(1)
+            WebDriverWait(driver2, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "place-bet"))).click() # not working
+            seed_before_click = the_seeds[-1]
+            print("entered situation 1")
+            #desktop_notification()
+            #discord_notification()
+            send_discord_msg(token, chanel_id, f"Its a Good time to bet! There have been {number_of_rounds_whitout_white + message_counter} rounds without white")
+            message_counter += 1
 
-write_to_csv(dictionary)
+    # Checks for how many whites have been selected in the last 30 rounds
+    num_of_whites = the_colors[-30:].count("white") #number_of_rounds_whitout_white
 
-color_list = []
-for i in dictionary:
-    color_list.append(dictionary[i])
-color_list.reverse()
+    # If the last color selected is white, bets on white again
+    if list(the_colors)[-1] == "white" and num_of_whites < 2 and len(dictionary) > 30:
+        if seed_before_click != last_seed:
+            sleep(1)
+            WebDriverWait(driver2, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "place-bet"))).click()#not working
+            seed_before_click = the_seeds[-1]
+            print("entered situation 2")
+            investo_counter = 1
+            message_counter = 0
 
-plot_results(color_list)
+    # After a white token has been selected continues betting on white for a set amount of rounds
+    if investo_counter > 0 and num_of_whites < 2:
+        if investo_counter >= number_of_bets_after_white:
+            investo_counter = 0
 
-play_double(color_list)
+        if seed_before_click != last_seed:
+            WebDriverWait(driver2, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "place-bet"))).click() #working
+            seed_before_click = the_seeds[-1]
+            investo_counter += 1
+            print("entered situation 3")
+
+    if num_of_whites >= 2:
+        investo_counter = 0
+
+    # Stores data to .txt file for data analysis
+    write_to_txt(the_colors)
+
+# plot_results(the_colors)
+
+# play_double(the_colors)
 
 driver.quit()
+driver2.quit()
